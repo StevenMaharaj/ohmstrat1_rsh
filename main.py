2410adc7fe743c626998815bbb252260eea61e1b
stevenmaharaj <stevesamah@gmail.com>
2024-02-28 20:20:20 +1100

    filtered products

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import pandas as pd
 
 
 def main():
-    print("Hello World!")
-    df = pd.read_csv('data.csv')
-    print(df.head())
+    print("Reading CSV file")
+    df = pd.read_csv('gioex.csv')
+    df = df[["name","quanto_multiplier","mark_price","order_size_min"]]
+    df["value_usd"] = df["mark_price"] * df["quanto_multiplier"] * df["order_size_min"]
+    df = df.sort_values(by="value_usd")
+    df.to_csv("sym_table.csv")
+    print(df.head(15))
+
+    # df_final = df.head(15)
+
+
+
+
+if __name__ == "__main__":
+    main()

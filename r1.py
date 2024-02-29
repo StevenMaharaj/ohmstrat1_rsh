5ae48828be4c798fc470ae117fc355df3716fdcd
stevenmaharaj <stevesamah@gmail.com>
2024-02-29 15:27:31 +1100

    choose more common products

--- r1.py
+++ r1.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 df = pd.read_csv('./sym_table.csv')
 
-assets = ["GALA","CAKE","SHIB","DASH","BNB","ICP"]
+# assets = ["GALA","CAKE","SHIB","DASH","BNB","ICP"]
+assets = ["BTC","ETH","BNB","ADA","XRP","DOGE","DOT","UNI","LTC","LINK","BCH","XLM","FIL","TRX","EOS"]
 
 symbols = map(lambda x: x + "_USDT", assets)
 
@@ -13,4 +14,7 @@ symbols = map(lambda x: x + "_USDT", assets)
 df = df[df['name'].isin(symbols)]
 
 
-print(df)
+# print(df)
+
+df_filtered = df[df['value_usd'] < 1.0]
+print(df_filtered)
